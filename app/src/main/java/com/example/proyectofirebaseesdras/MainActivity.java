55044d16cd9a4d6060c4aad074094d33eed40a16
Exdras007 <exdrasooo007@outlook.es>
2023-02-09 21:02:56 +0100

    V0.1 Inicio y registro de sesion completados

--- app/src/main/java/com/example/proyectofirebaseesdras/MainActivity.java
+++ app/src/main/java/com/example/proyectofirebaseesdras/MainActivity.java
@@ -1,9 +1,18 @@
 package com.example.proyectofirebaseesdras;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
-import android.widget.EditText;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
@@ -14,8 +23,20 @@ public class MainActivity extends AppCompatActivity
     FirebaseDatabase database;
     private FirebaseAuth mAuth;
     private FirebaseUser currentUser;
-    private EditText edt_nombreusuario;
+    private EditText edt_emailusuario;
     private EditText edt_claveUsuario;
+
+    @Override
+    public void onStart()
+    {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        currentUser = mAuth.getCurrentUser();
+        if(currentUser != null)
+        {
+            currentUser.reload();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -23,14 +44,89 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // ----------------------------------------
-        edt_nombreusuario = (EditText) findViewById(R.id.edt_nombre);
+        edt_emailusuario = (EditText) findViewById(R.id.edt_email);
         edt_claveUsuario = (EditText) findViewById(R.id.edt_clave);
-
+        // ---
         mAuth = FirebaseAuth.getInstance();
-
+        // Pruebas en firebase realtime
         database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference();
 
-        myRef.child("saludo").setValue("Hola de nuevo");
+        // myRef.child("saludo").setValue("Hola de nuevo");
+    }
+
+    public void loguearUsuario(View view)
+    {
+        String email = String.valueOf(edt_emailusuario.getText());
+        String clave = String.valueOf(edt_claveUsuario.getText());
+
+        if(email.isEmpty())
+        {
+            edt_emailusuario.setError("Debes poner tu email");
+            return;
+        }
+        else if(clave.length()<5)
+        {
+            edt_claveUsuario.setError("La clave no es correcta");
+            return;
+        }
+        else
+        {
+            mAuth.signInWithEmailAndPassword(email, clave)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Toast.makeText(MainActivity.this, "Inicio de sesion correcto, bienvenido", Toast.LENGTH_SHORT).show();
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                Intent intent = new Intent(MainActivity.this, Inicio.class);
+                                startActivity(intent);
+                            } else {
+                                // Si falla el inicio de sesion, avisar al usuario
+                                Toast.makeText(MainActivity.this, "Error al iniciar sesion", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        }
+    }
+
+    public void registrarUsuario(View view)
+    {
+        String email = String.valueOf(edt_emailusuario.getText()).trim();
+        String clave = String.valueOf(edt_claveUsuario.getText());
+
+        if(email.isEmpty())
+        {
+            edt_emailusuario.setError("El email no puede ser nulo");
+            return;
+        }
+        else if(clave.length()<5)
+        {
+            edt_claveUsuario.setError("La clave debe tener al menos 6 caracteres");
+            return;
+        }
+        else
+        {
+            mAuth.createUserWithEmailAndPassword(email, clave)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>()
+                    {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task)
+                        {
+                            if (task.isSuccessful())
+                            {
+                                // Registro completado, update UI with the signed-in user's information
+                                Toast.makeText(MainActivity.this, "Usuario registrado correctamente", Toast.LENGTH_SHORT).show();
+                            }
+                            else
+                            {
+                                // Si el logeo falla, mandas un mensaje al usuario
+                                Toast.makeText(MainActivity.this, "No se pudo registrar al usuario", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        }
+
     }
 }
